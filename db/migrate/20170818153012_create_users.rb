daad402add17a56c91f5896fadd201fde3abcdfa
kaalm1 <kaalm1@gmail.com>
2017-08-21 12:46:12 -0400

    navbar

--- db/migrate/20170818153012_create_users.rb
+++ db/migrate/20170818153012_create_users.rb
@@ -6,7 +6,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.string :email
       t.string :password_digest
       t.integer :phone_number
-      t.string :profile_image_url
+      t.string :profile_image_url, default: "http://thecampanile.org/wp-content/uploads/2016/10/blank-profile.jpg"
 
       t.timestamps
     end
