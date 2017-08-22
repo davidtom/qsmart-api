3db8bab3b79ae4cccb7a49932200761d9bd63d99
wbdana <william.b.dana@gmail.com>
2017-08-22 17:38:34 -0400

    Working UserShowPage websockets for userPlace and userCount on each line

--- app/controllers/api/v1/lines_controller.rb
+++ app/controllers/api/v1/lines_controller.rb
@@ -14,7 +14,7 @@ class Api::V1::LinesController < ApplicationController
     # ActionCable.server.broadcast "line_channel_#{line_id}", @line.users
     # ActionCable.server.broadcast_to(@line, @line.waiting_users)
     sleep(0.25)
-    LineJoinedChannel.broadcast_to(@line_joined, @line.waiting_users)
+    LineJoinedChannel.broadcast_to(@line, @line.waiting_users)
     LineChannel.broadcast_to(@line, @line.waiting_users)
   end
 
