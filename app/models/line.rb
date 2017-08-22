7dac3e12252daadaa688969ba464f04188e864af
davidtom <davidjtomczyk@gmail.com>
2017-08-22 09:54:12 -0400

    simplify join query

--- app/models/line.rb
+++ app/models/line.rb
@@ -29,9 +29,9 @@ class Line < ApplicationRecord
     Line.all.pluck(:code).include?(code) ? generate_code : self.code = code
   end
 
-  def waiting_users()
+  def waiting_users
     a = ActiveRecord::Base.connection
-    result = a.execute(%Q{SELECT users.id, users.first_name, users.last_name, users.email, users.phone_number, users.profile_image_url, lines_users.waiting  FROM users JOIN lines_users ON users.id = lines_users.user_id JOIN lines on lines.id = lines_users.line_id WHERE waiting=true AND lines.id=#{a.quote(self.id)};})
+    result = a.execute(%Q{SELECT users.id, users.first_name, users.last_name, users.email, users.phone_number, users.profile_image_url, lines_users.waiting  FROM users JOIN lines_users ON users.id = lines_users.user_id WHERE waiting=true AND lines_users.line_id=#{a.quote(self.id)};})
     result
     # sql = <<-sql
     # SELECT users.id, users.first_name, users.last_name, users.email,
