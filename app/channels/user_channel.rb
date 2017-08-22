424392210479d2562739457c4a2c1858b9cdb6ce
wbdana <william.b.dana@gmail.com>
2017-08-22 16:28:00 -0400

    Working UserShow websocket for total users in each line

--- app/channels/user_channel.rb
+++ app/channels/user_channel.rb
@@ -8,10 +8,14 @@ class UserChannel < ApplicationCable::Channel
   def received(data)
     # line_total = @user.lines.map{|line| line.users.count}
 <<<<<<< HEAD
+<<<<<<< HEAD
     UserChannel.broadcast_to(@user, {lines: current_user.lines, createdLines: current_user.created_lines})
 =======
     UserChannel.broadcast_to(@user, @user.lines)
 >>>>>>> Initial commit
+=======
+    UserChannel.broadcast_to(@user, {lines: current_user.lines, createdLines: current_user.created_lines})
+>>>>>>> Working UserShow websocket for total users in each line
   end
 
   def unsubscribed
