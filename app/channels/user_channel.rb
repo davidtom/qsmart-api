7904fc70de84d638035595f8025669b039bfadb8
wbdana <william.b.dana@gmail.com>
2017-08-22 14:31:40 -0400

    Initial commit

--- app/channels/user_channel.rb
+++ app/channels/user_channel.rb
@@ -7,7 +7,11 @@ class UserChannel < ApplicationCable::Channel
 
   def received(data)
     # line_total = @user.lines.map{|line| line.users.count}
+<<<<<<< HEAD
     UserChannel.broadcast_to(@user, {lines: current_user.lines, createdLines: current_user.created_lines})
+=======
+    UserChannel.broadcast_to(@user, @user.lines)
+>>>>>>> Initial commit
   end
 
   def unsubscribed
