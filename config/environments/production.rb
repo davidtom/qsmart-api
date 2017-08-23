4d71068d518879cbdc3b98ddd34d9d10e15ece53
wbdana <william.b.dana@gmail.com>
2017-08-23 18:58:07 -0400

    add local host request origin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   config.action_cable.mount_path = '/cable'
   config.action_cable.url = 'wss://qsmart-api.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://qsmart-react.herokuapp.com', /https:\/\/qsmart-react.herokuapp.*/, 'http://qsmart-react.herokuapp.com', /http:\/\/qsmart-react.herokuapp.*/  ]
+  config.action_cable.allowed_request_origins = [ 'https://qsmart-react.herokuapp.com', /https:\/\/qsmart-react.herokuapp.*/, 'http://qsmart-react.herokuapp.com', /http:\/\/qsmart-react.herokuapp.*/, /http:\/\/localhost:*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
