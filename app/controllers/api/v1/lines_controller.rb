d54ffe320aeb5c1065288b96482ed71aa486d618
kaalm1 <kaalm1@gmail.com>
2017-08-23 13:46:37 -0400

    activate

--- app/controllers/api/v1/lines_controller.rb
+++ app/controllers/api/v1/lines_controller.rb
@@ -18,4 +18,9 @@ class Api::V1::LinesController < ApplicationController
     @users = Line.find(params[:id]).users
     render json: @users
   end
+
+  def update
+    @line = Line.find(params[:id])
+    @line.update(active: !@line.active)
+  end
 end
