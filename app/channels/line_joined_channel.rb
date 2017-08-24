9f395b834da17c0814f510a21eb365fe1fdd453e
wbdana <william.b.dana@gmail.com>
2017-08-24 00:14:28 -0400

    Remove user_channel.rb, add unsubscribe to channels, update line_joined_channel for userPlace

--- app/channels/line_joined_channel.rb
+++ app/channels/line_joined_channel.rb
@@ -1,15 +1,14 @@
 class LineJoinedChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
     @line = Line.find_by(id: params[:room])
     stream_for @line
   end
 
   def received(data)
-    LineJoinedChannel.broadcast_to(@line, @line.waiting_users)
+    LineJoinedChannel.broadcast_to(@line, {line: @line, users: @line.waiting_users})
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    stop_all_streams
   end
 end
