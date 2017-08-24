815491d8887748b9700ce71122be23c52207806b
wbdana <william.b.dana@gmail.com>
2017-08-23 23:39:01 -0400

    Update README, clean up lines_controller

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ A next gen queueing API. Get in line to join the future!
 
 What can your queue do for you?
 
-newac-merge-master
+We're live! Visit https://qsmart-react.herokuapp.com to get started.
--- app/controllers/api/v1/lines_controller.rb
+++ app/controllers/api/v1/lines_controller.rb
@@ -4,7 +4,6 @@ class Api::V1::LinesController < ApplicationController
     image_url = params[:imageURL] || "http://imgur.com/n00Ed17.jpg"
     @line = Line.create(name: params[:name], image_url: image_url)
     current_user.created_lines << @line
-    # sleep(0.5)
     LineJoinedChannel.broadcast_to(@line, @line.waiting_users)
     LineChannel.broadcast_to(@line, {line: @line, users: @line.waiting_users})
     render json: {line: @line}, status: 200
@@ -13,9 +12,6 @@ class Api::V1::LinesController < ApplicationController
   def show
     line_id = params[:id]
     @line = Line.find(line_id)
-    # ActionCable.server.broadcast "line_channel_#{line_id}", @line.users
-    # ActionCable.server.broadcast_to(@line, @line.waiting_users)
-    # sleep(0.5)
     LineJoinedChannel.broadcast_to(@line, @line.waiting_users)
     LineChannel.broadcast_to(@line, {line: @line, users: @line.waiting_users})
     render json: {line: @line, users: @line.waiting_users}
