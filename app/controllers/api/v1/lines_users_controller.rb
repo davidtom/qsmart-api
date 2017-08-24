e293d6a3d97d612c8105da7bd1707210ba745d3a
wbdana <william.b.dana@gmail.com>
2017-08-23 20:32:35 -0400

    after_commit :broadcast test

--- app/controllers/api/v1/lines_users_controller.rb
+++ app/controllers/api/v1/lines_users_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::LinesUsersController < ApplicationController
   before_action :authenticate_user
+  after_commit :broadcast, on: [:create, :update, :destroy]
+
+  def broadcast
+    sleep(0.5)
+    LineJoinedChannel.broadcast_to(@line, @line.waiting_users)
+    LineChannel.broadcast_to(@line, {line: @line, users: @line.waiting_users})
+  end
 
   def create
     @user = current_user
@@ -8,9 +15,9 @@ class Api::V1::LinesUsersController < ApplicationController
       @record = LinesUser.new(user_id: @user.id, line_id: @line.id)
       if @record.save
         send_create_text(@line)
-        sleep(1.0)
-        LineJoinedChannel.broadcast_to(@line, @line.waiting_users)
-        LineChannel.broadcast_to(@line, {line: @line, users: @line.waiting_users})
+        # sleep(1.0)
+        # LineJoinedChannel.broadcast_to(@line, @line.waiting_users)
+        # LineChannel.broadcast_to(@line, {line: @line, users: @line.waiting_users})
         render json: {line_id: @line.id}, status: 200
       elsif @line.active == false
         render json: {error: "Line is not active at this time", line: @line}, status: 422
@@ -30,9 +37,9 @@ class Api::V1::LinesUsersController < ApplicationController
     if @record.update(waiting: false)
       send_text(Line.find(params[:line]))
       @line = Line.find(params[:line])
-      sleep(1.0)
-      LineJoinedChannel.broadcast_to(@line, @line.waiting_users)
-      LineChannel.broadcast_to(@line, {line: @line, users: @line.waiting_users})
+      # sleep(1.0)
+      # LineJoinedChannel.broadcast_to(@line, @line.waiting_users)
+      # LineChannel.broadcast_to(@line, {line: @line, users: @line.waiting_users})
       render json: {}, status: 204
     else
       render json: {error: "unable to update"}, status: 500
@@ -46,9 +53,9 @@ class Api::V1::LinesUsersController < ApplicationController
     if @record.destroy
       send_text(Line.find(params[:line]))
       @line = Line.find(params[:line])
-      sleep(1.0)
-      LineJoinedChannel.broadcast_to(@line, @line.waiting_users)
-      LineChannel.broadcast_to(@line, {line: @line, users: @line.waiting_users})
+      # sleep(1.0)
+      # LineJoinedChannel.broadcast_to(@line, @line.waiting_users)
+      # LineChannel.broadcast_to(@line, {line: @line, users: @line.waiting_users})
       render json: {}, status: 204
     else
       render json: {error: "unable to delete"}, status: 500
